45af816aa049fa09359948a73d49042cfb43d8f9
boyiechen <boyie@uchicago.edu>
2023-04-16 03:01:49 +0000

    learn dynamic route

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -7,4 +7,14 @@ class CurrenciesController < ApplicationController
     @array_of_symbols = @symbols_hash.keys()
     render({ :template => "currency_templates/step_one.html.erb" })
   end
+
+  def second_currency
+    @first_currency_symbol = params.fetch("first_currency_symbol")
+    @raw_data = open("https://api.exchangerate.host//symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys()
+    render({ :template => "currency_templates/step_two.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/forex", { :controller => "currencies", :action => "first_currency" })
+
+  # dynamic routes
+  get("/forex/:first_currency_symbol", { :controller => "currencies", :action => "second_currency" })
 end
